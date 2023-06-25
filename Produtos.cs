f4bbb178318bb75a292d422bbdedd405ec7e588b
Bruno Barbosa <42818756+Brunocorreiabarbosa@users.noreply.github.com>
2023-06-24 23:32:24 -0300

    Add project files.

---
+++ Produtos.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+
+namespace Mercado
+{
+    internal class Produtos
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produtos(string nome, double preco,int quantidade) 
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+           
+        }
+        public Produtos(string nome, double preco) 
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 5;
+        }
+        public double ValorTotalEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos( int quant)
+        {
+           
+          if (quant >= 1)
+            {
+                this.Quantidade += quant;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade incorreta");
+            }
+
+        }
+
+        public void RemoverProdutos(int quant)
+        {
+            if (Quantidade >= quant)
+            {
+               Quantidade-= quant;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade Indisponivel");  
+            }
+        }
+        public override string ToString()
+        {
+            return $"======================Dados Do Produto===================\n" +
+                $"Produto: {Nome}\n" +
+                $"Quantidade:{Quantidade}\n" +
+                $"Preço unitario: {Preco.ToString("F2",CultureInfo.InvariantCulture)}\n" +
+                $"Valor total No Estoque: R$:{ValorTotalEstoque().ToString("F2",CultureInfo.InvariantCulture)}\n" +
+                $"==========================================================\n";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Produtos produtos = new Produtos();
+
+            Console.WriteLine("Digite o nome do produto para adicionalo:");
+            string nome = (Console.ReadLine());
+
+            /*Console.WriteLine($"Digite a quantidade de {nome} que tem no estoque ");
+            int quantidade = int.Parse(Console.ReadLine());*/
+
+            Console.WriteLine($"Digite o valor de cada {nome}.");
+            double preco = double.Parse(Console.ReadLine());          
+            Produtos produtos = new Produtos(nome,preco);
+            Console.WriteLine(produtos);
+            Console.WriteLine($"Deseja remover produtos Digite 1 para sim e 0 para nao:");
+            string resposta = Console.ReadLine();
+
+            if (resposta == "1")
+            {
+                Console.WriteLine("Digite a quantidade que Deseja remover");
+                int quanti = int.Parse(Console.ReadLine());
+                produtos.RemoverProdutos(quanti);
+                Console.WriteLine(produtos);
+                Console.WriteLine("Obrigado Por Usar O Nossos Sistema ate breve ...");
+                Console.WriteLine("Aperte qualquer tecla Para sair...");
+                Console.ReadKey();
+            }
+            else if (resposta == "0")
+            {
+                Console.WriteLine("Obrigado Por Usar O Nossos Sistema ate breve ...");
+                Console.WriteLine("Aperte qualquer tecla Para sair...");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Comando Invalido");
+                Console.WriteLine("Aperte qualquer tecla Para sair...");
+                Console.ReadKey();
+            }
+            
+            
+        }
+    }
+}
